9a88cb2be0d0a8ede7e3564fc8d739c758a35bc0
Abdo Sabry <abdo0@DESKTOP-E8K7FG2>
2022-07-23 00:15:59 +0200

    add job views & urls

--- job/models.py
+++ job/models.py
@@ -1,6 +1,13 @@
 from random import choices
 from statistics import mode
+from tkinter import CASCADE
 from django.db import models
+
+'''
+    -html widget
+    -validation
+    -db size
+'''
 
 JOB_TYPES = (
     ('FULL TIME','FULL TIME'),
@@ -18,9 +25,12 @@ class Job(models.Model):  #table name Job
     vacancy = models.IntegerField(default=1)
     salary = models.IntegerField(default=1)
     experience = models.IntegerField(default=1)
-    
-
-
-
+    category = models.ForeignKey('Category',on_delete=models.CASCADE,blank=True)
     def __str__(self):
         return self.title
+
+
+class Category(models.Model):
+    Category_name = models.CharField(max_length=30)
+    def __str__(self):
+        return self.Category_name
