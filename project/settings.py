175a98be6a4e33f530bb9458c93f29919b706a48
Abdelrahman Sabry <75238226+A-132001@users.noreply.github.com>
2022-10-29 17:49:36 +0200

    To Deployment files

--- project/settings.py
+++ project/settings.py
@@ -41,7 +41,7 @@ INSTALLED_APPS = [
     #Apps
     'api',
     'job',
-    "bootstrap4",
+    'bootstrap4',
     'django_filters',
     'contact',
     'rest_framework',
@@ -49,6 +49,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -122,12 +123,13 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
-
+import os
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     BASE_DIR / "static",
     '/var/www/static/',
 ]
+STATIC_ROOT=os.path.join(BASE_DIR, 'static')
 
 MEDIA_URL = 'media/'
 MEDIA_ROOT = BASE_DIR / "media"
